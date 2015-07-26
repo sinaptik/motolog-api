a94696f61a8342ace319c55dea6cbce578c6c353
sinaptik <sin486@gmail.com>
2015-07-26 19:19:27 +1200

    add get oil changes api

--- api/controllers/MaintenanceController.js
+++ api/controllers/MaintenanceController.js
@@ -9,6 +9,22 @@ module.exports = {
     //store and retrieve maintenence items for each car, for example
     //oil changes, brake disc changes, wofs
 
+    getOilChanges: function (req, res) {
+        var vehicleId = req.param('vehicle');
+
+        if (!vehicleId) {
+            res.status(400).end();
+        }
+
+        Maintenance.find({
+            owner: req.userId,
+            vehicle: vehicleId,
+            type: 'oilchange'
+        }, function (err, result) {
+            res.json(result);
+        })
+    },
+
     addMaintenance: function (req, res) {
         User.findOne(req.userId, function (err, user) {
 
